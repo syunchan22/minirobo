fc9d325175166d174e0ad0653af4f9b4bda934ea
syunchan <syunya101425@gmail.com>
2021-10-23 18:21:59 +0900

    もーどのきりかえができました。

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,57 @@
 #include <Arduino.h>
-const int ain1_pin = 18;
-const int ain2_pin = 17;
-const int bin1_pin = 16;
-const int bin2_pin = 4;
 
-const uint8_t ain1_ch = 0;
-const uint8_t ain2_ch = 1;
-const uint8_t bin1_ch = 2;
-const uint8_t bin2_ch = 3;
+enum Pattern{ //enum 列挙体 0,1,...
+  INIT,
+  TEST,
+  MOTOR_TEST,
+};
+Pattern pattern;
+
+String modelist(){
+  String str;
+  str += "  " + String(Pattern::INIT) + ":Initial mode\n";
+  str += "  " + String(Pattern::TEST) + ":Test mode\n";
+  str += "  " + String(Pattern::MOTOR_TEST) + ":Motor Test mode\n";
+  return str;
+};
+
+void setup(){
+  Serial.begin(9600);//[b/s]
+  Serial.setTimeout(100000);
+  pattern = Pattern::INIT;
+  delay(5000);
+  Serial.println("\n\n\n\n");
+};
 
 
+void loop(){
+  String str;
+  
+  switch (pattern)
+  {
+  case Pattern::INIT:
+    Serial.println("select mode:");
+    Serial.println(modelist());
+    str = Serial.readStringUntil('\n');
+    pattern = (Pattern)str.toInt();
+    Serial.println(str);
 
+    break;
 
-uint8_t pwm_init(const int pin, const uint8_t ch){
-  ledcSetup(ch, 500000, 8);
-  ledcAttachPin(pin, ch);
-  return ch;
-}
+  case Pattern::TEST:
+    Serial.println("test mode:");
+    pattern = Pattern::INIT;
 
-void motor_update(const float speed, const int ch1, const int ch2){
-  if(speed > 0){
-    ledcWrite(ch2, 50);
-    ledcWrite(ch1, 0);
-  }else{
-    ledcWrite(ch1, 0);
-    ledcWrite(ch2, 0);
+    break;
+
+  case Pattern::MOTOR_TEST:
+    Serial.println("Motor test mode:");
+
+    pattern = Pattern::INIT;
+  
+  default:
+    Serial.println("return initial");
+    pattern = Pattern::INIT;
+    break;
   }
-}
-
-void setup() {
-  // put your setup code here, to run once:
-  pwm_init(ain1_pin, ain1_ch);
-  pwm_init(ain2_pin, ain2_ch);
-  pwm_init(bin1_pin, bin1_ch);
-  pwm_init(bin2_pin, bin2_ch);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  motor_update(0.7, ain1_ch, ain2_ch);
-  motor_update(0.7, bin1_ch, bin2_ch);
-}
+};
